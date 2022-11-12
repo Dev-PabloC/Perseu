4866ecb50d99fbea5ef072efd4d4eb130b7f21c7
Pablo h.c <pablo.dev.carvalho@gmail.com>
2022-11-12 13:46:10 -0300

    Erase frotend dir and fixing readme

--- frontend/src/graphql/client.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const client = new ApolloClient({
-	uri: "https://g9lgns-4000.preview.csb.app/",
-	cache: new InMemoryCache(),
-});
--- frontend/src/pages/auth/auth.tsx
+++
@@ -1,8 +0,0 @@
-export default {
-	login: () => {
-		return <></>;
-	},
-	register: () => {
-		return <></>;
-	},
-};
